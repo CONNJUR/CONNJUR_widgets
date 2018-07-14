f47c31cc0905d9585089e13069c50d0c1a829821
mgryk <mgryk@feb09.nmrbox.org>
2018-07-14 10:49:39 -0400

    code cleanup

--- cjrShowProcpar/main.c
+++ cjrShowProcpar/main.c
@@ -25,29 +25,25 @@
  */
 int main(int argc, char** argv) {
 
-    if (argc != 2) {printf("Usage: %s filename\n", argv[0]); return 1;}
-    
     GtkBuilder *builder;
     GtkWidget *window;
+    GtkListStore *procparStore;
+    GtkTreeView *procparView;
     GError *error = NULL;
+    
+    if (argc != 2) {printf("Usage: %s filename\n", argv[0]); return 1;}
 
     gtk_init(&argc, &argv);
 
     builder = gtk_builder_new();
-	
     if (!gtk_builder_add_from_file(builder, "/home/nmrbox/mgryk/CONNJUR_spike/cjrShowProcpar/procpar.glade", &error)) 
 	{
 		g_warning("%s", error->message);
 		g_free(error);
 		return(1);
 	}
-
     window = GTK_WIDGET(gtk_builder_get_object(builder, "procparWindow"));
-
     gtk_builder_connect_signals(builder, NULL);
-    
-    GtkListStore *procparStore;
-    GtkTreeView *procparView;
     
     procparView = GTK_TREE_VIEW(gtk_builder_get_object(builder,"procparTreeView"));
     procparStore = parseProcpar(argv[1]);
@@ -56,7 +52,6 @@ int main(int argc, char** argv) {
     gtk_tree_view_set_search_entry(procparView,GTK_ENTRY(gtk_builder_get_object(builder,"procparSearchEntry")));
     
     g_object_unref(G_OBJECT(builder));
-
     gtk_widget_show(window);
 
     gtk_main();
